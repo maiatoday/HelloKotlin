1b4055468adc3802091f212a1e61e09d97591772
maiatoday <maiatoday@gmail.com>
2015-11-08 19:52:40 +0200

    more libs

--- app/src/main/java/net/maiatoday/hellokotlin/MainActivity.kt
+++ app/src/main/java/net/maiatoday/hellokotlin/MainActivity.kt
@@ -5,9 +5,9 @@ import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
-import android.view.View
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,6 +23,7 @@ class MainActivity : AppCompatActivity() {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show()
             }
         })
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
